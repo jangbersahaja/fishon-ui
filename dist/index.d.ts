06ccdcb9051fc9ae34c07cc82049d87559cedfe3
jangbersahaja <mmuter4@gmail.com>
2025-11-25 08:38:47 +0800

    Refactor code structure for improved readability and maintainability

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
 export type { Captain, Charter, CharterSchedule, FishingType, Pickup, Policies, Tier, Trip, UnavailabilityPeriod, } from "./types/charter";
 export * from "./components/charter";
+export * from "./components/icons";
 export * from "./data/amenities";
 export * from "./data/species";
 export * from "./lib/formatters";
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,8 @@
 // Main entry point for @fishon/ui package
 // Export charter components
 export * from "./components/charter";
+// Export icons
+export * from "./components/icons";
 // Export data utilities
 export * from "./data/amenities";
 export * from "./data/species";
